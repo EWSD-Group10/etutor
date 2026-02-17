13f77ee0119db974cf85f4523d47c2a539cd5347
Saw Win Naung <sawwinnnaung@gmaill.com>
2026-02-16 19:28:58 -0500

    role based access control for tutor and admin

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken"
+import { pool } from "../utils/db.js"
 
 export const requireSignin = (req, res, next) => {
   try {
@@ -18,5 +19,37 @@ export const requireSignin = (req, res, next) => {
   } catch (err) {
     console.error(err)
     return res.status(401).json({ error: "Invalid or expired token" })
+  }
+}
+
+export const isInstructor = async (req, res, next) => {
+  try {
+    const result = await pool.query("SELECT role FROM users WHERE id = $1", [
+      req.auth._id,
+    ])
+    const user = result.rows[0]
+    if (!user || !user.role.includes("Instructor")) {
+      return res.sendStatus(403)
+    } else {
+      next()
+    }
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export const isAdmin = async (req, res, next) => {
+  try {
+    const result = await pool.query("SELECT role FROM users WHERE id = $1", [
+      req.auth._id,
+    ])
+    const user = result.rows[0]
+    if (!user || !user.role.includes("Admin")) {
+      return res.sendStatus(403)
+    } else {
+      next()
+    }
+  } catch (err) {
+    console.log(err)
   }
 }
